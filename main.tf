24579517d77217459e579c342a96d96f691d2622
Ganesh-patrana <ganeshpatrana@gmail.com>
2026-05-31 02:29:03 +0000

    Day 21: Provisioning Azure Container Registry and AKS RBAC permissions

--- main.tf
+++ main.tf
@@ -77,4 +77,23 @@ resource "azurerm_kubernetes_cluster" "aks" {
     service_cidr      = "192.168.0.0/16"
     dns_service_ip    = "192.168.0.10"
   }
+
+  # 5. Build the Azure Container Registry (The Vault)
+resource "azurerm_container_registry" "acr" {
+  # NOTE: ACR names must be globally unique across all of Azure! 
+  # Change the numbers below to something random so it doesn't collide with anyone else.
+  name                = "patrana345118" 
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# 6. Give Kubernetes Permission to pull from the Vault
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
 }
