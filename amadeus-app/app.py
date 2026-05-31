c607b7baa3ed5cf65db704cd90118fda4f47c021
Ganesh-patrana <ganeshpatrana@gmail.com>
2026-05-31 04:14:11 +0000

    Day 21: Pushing Amadeus Flight API v1.0.0 and CI Pipeline

---
+++ amadeus-app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+# This simulates a version change that we will trigger later via CI/CD
+APP_VERSION = "1.0.0"
+
+@app.route('/')
+def home():
+    return jsonify({
+        "company": "Amadeus",
+        "service": "Flight Search Engine",
+        "version": APP_VERSION,
+        "status": "Healthy",
+        "message": "Welcome to the Amadeus Global Distribution System"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
